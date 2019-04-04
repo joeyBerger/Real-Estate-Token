1d802d684fcc2822f7cde10e78443944854812cb
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2019-04-03 20:31:00 -0700

    before upgrading solidity

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -9,15 +9,37 @@ import "./Oraclize.sol";
 contract Ownable {
     //  TODO's
     //  1) create a private '_owner' variable of type address with a public getter function
+    address private _owner;
     //  2) create an internal constructor that sets the _owner var to the creater of the contract 
+    constructor() public {
+        _owner = msg.sender;
+        emit OwnershipTransfered(_owner);
+    }
     //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
-    //  4) fill out the transferOwnership function
+    //Function Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Account must belong to contract owner");
+        _;
+    }
+
+    modifier isValidAddress(address newOwner) {
+        //bool something = newOwner.isContract();
+        //require(newOwner.isContract() == false, "Sending account must not be a contract address");  //need to investigate temp
+        require(newOwner != address(0), "Sending account must not be invalid");
+        _;
+    }
+    
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
 
-    function transferOwnership(address newOwner) public onlyOwner {
+    //Events
+    event OwnershipTransfered(address newOwner);
+
+    //  4) fill out the transferOwnership function
+    //Functions
+    function transferOwnership(address newOwner) public onlyOwner isValidAddress(newOwner) {
         // TODO add functionality to transfer control of the contract to a newOwner.
         // make sure the new owner is a real address
-
+        _owner = newOwner;
     }
 }
 
@@ -27,6 +49,9 @@ contract Ownable {
 //  3) create an internal constructor that sets the _paused variable to false
 //  4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
 //  5) create a Paused & Unpaused event that emits the address that triggered the event
+contract Pausable is Ownable {
+
+}
 
 contract ERC165 {
     bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
@@ -416,9 +441,12 @@ contract ERC721Enumerable is ERC165, ERC721 {
 contract ERC721Metadata is ERC721Enumerable, usingOraclize {
     
     // TODO: Create private vars for token _name, _symbol, and _baseTokenURI (string)
-
+    string private _name;
+    string private _symbol;
+    string private _baseTokenURI;
     // TODO: create private mapping of tokenId's to token uri's called '_tokenURIs'
-
+    // uint256 tokenId;  //TODO: this probably is not right
+    // mapping(uint256 => tokenId) private _tokenURIs;
     bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
     /*
      * 0x5b5e139f ===
@@ -430,15 +458,24 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 
     constructor (string memory name, string memory symbol, string memory baseTokenURI) public {
         // TODO: set instance var values
-
+        _name = name;
+        _symbol = symbol;
+        _baseTokenURI = baseTokenURI;
         _registerInterface(_INTERFACE_ID_ERC721_METADATA);
     }
 
     // TODO: create external getter functions for name, symbol, and baseTokenURI
+    function getERC721Data() external returns (string memory name, string memory symbol, string memory baseTokenURI) {
+        name = _name;
+        symbol = _symbol;
+        baseTokenURI = _baseTokenURI;
+    }
 
     function tokenURI(uint256 tokenId) external view returns (string memory) {
         require(_exists(tokenId));
-        return _tokenURIs[tokenId];
+        string memory shit = "fuck";
+        return shit; //temp
+        //return _tokenURIs[tokenId];   //TODO: properly implement this return value
     }
 
 
---
+++ eth-contracts/truffle.js
@@ -0,0 +1,56 @@
+// var HDWalletProvider = require("truffle-hdwallet-provider");
+// var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+// mnemonic = "tell foil harbor summer people put woman country opinion write first switch";
+// // use to fire up ganache
+// // ganache-cli --mnemonic "tell foil harbor summer people put woman country opinion write first switch" -a 40
+// module.exports = {
+//   networks: {
+//     development: {
+//       provider: function() {
+//         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+//       },
+//       network_id: '*',
+//       //gas: 9999999
+//     }
+//   },
+//   compilers: {
+//     solc: {
+//       version: "^0.4.24"
+//     }
+//   }
+// };
+
+
+// Allows us to use ES6 in our migrations and tests.
+// require('babel-register')
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'spirit supply whale amount human item harsh scare congress discover talent hamster';
+mnemonic = "tell foil harbor summer people put woman country opinion write first switch";
+module.exports = {
+  networks: { 
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    }, 
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/bfda5968427443ab86ee3ca5fd20270b') 
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  }
+};
+// module.exports = {
+//   networks: {
+//       development: {
+//           host: "localhost",
+//           port: 8545,
+//           network_id: "*" // Match any network id
+//       }
+//   }
+// };
