cde693d0b685a07b24bc1694c23b525e01f1bf6a
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2019-04-09 07:45:43 -0700

    verifier contract upgraded

---
+++ eth-contracts/contracts/test.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+// pragma experimental ABIEncoderV2;
+
+contract testContract {
+
+    struct myStruct {
+        uint256 something;
+    }
+
+    uint256 [] arr;
+
+    constructor() public {
+
+    }
+
+    // function testFunc(myStruct memory d) public {
+
+    // }
+
+
+    // function testFunc(uint [] memory jo) public {
+
+    // }
+
+}
